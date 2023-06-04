75d1fb52cb3d2d817d413c1d79e6dcaba202bb12
Eduard Stepanyan <stepanyan.edua@gmail.com>
2023-06-04 11:57:10 +0300

    Initial commit

---
+++ src/main/java/com/steppedua/securityproject/domain/Role.java
@@ -0,0 +1,17 @@
+package com.steppedua.securityproject.domain;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+
+@RequiredArgsConstructor
+public enum Role implements GrantedAuthority {
+    ROLE_USER("ROLE_USER"),
+    ROLE_ADMIN("ROLE_ADMIN");
+
+    private final String value;
+
+    @Override
+    public String getAuthority() {
+        return value;
+    }
+}
